4bd81267aa87bd979acfc3ff2565a40365338032
Sim Neyzi <simneyzi@gmail.com>
2021-11-15 13:40:38 -0800

    add scroll function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 /**
  *
  * Manipulating the DOM exercise.
@@ -14,22 +15,27 @@
 */
 
 /**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  *
 */
-
-
+const sections = document.querySelectorAll('section');
+const navBar = document.querySelector('#navbar__list');
+let highlighted = document.querySelector('ul');
+let sectionObj = {};
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
-
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 
 /**
@@ -39,12 +45,43 @@
 */
 
 // build the nav
+function buildNav() {
+  const fragment = document.createDocumentFragment();
 
+  for (const section of sections) {
+    const li = document.createElement('li');
+    // const a = document.createElement('a');
+    li.textContent = section.dataset.nav;
+    sectionObj[section.dataset.nav] = section.id;
+    //console.log(sectionObj);
+    // a.setAttribute('href', '#'+section.id);
+    // li.appendChild(a);
+    fragment.appendChild(li);
+  }
 
+  navBar.appendChild(fragment);
+}
 // Add class 'active' to section when near top of viewport
-
+function addClassActive(elements) {
+  for (const element of elements) {
+    if (isInViewport(element)) {
+      element.classList.add('your-active-class');
+    } else {
+      element.classList.remove('your-active-class');
+    }
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
+
+function scroll(element) {
+  highlighted.classList.remove('highlight');
+  highlighted = element;
+  element.classList.add('highlight');
+  const id = sectionObj[element.innerText];
+  const clickedSection = document.getElementById(id);
+  clickedSection.scrollIntoView({behavior:'smooth'});
+}
 
 
 /**
@@ -53,7 +90,13 @@
  *
 */
 
+document.addEventListener('scroll', () => {addClassActive(sections)});
+navBar.addEventListener('click',
+  (e) => {e.target.nodeName === 'LI' ? scroll(e.target) : null}
+);
+
 // Build menu
+buildNav();
 
 // Scroll to section on link click
 
