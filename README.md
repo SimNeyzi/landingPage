65c3acf6d3ac9d860bcbaa082b448b315b883efa
Sim Neyzi <simneyzi@gmail.com>
2021-11-17 17:10:04 -0800

    hide navbar when not scrolling

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# landingPage
+# Landing Page
+______________
+
+#### Landing page features a dynamic navigation bar, scrolls to anchors from navigation, highlights section in viewport and hide/display the navigation bar upon scrolling.
+
+## Installing
+_____________
+
+*
+
+* Dependencies: None
+* JS Version: ES2015/ES6
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,32 +1,11 @@
-
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
-
-/**
- * Define Global Variables
- *
-*/
+// Global Variables
 const sections = document.querySelectorAll('section');
 const navBar = document.querySelector('#navbar__list');
 let highlighted = document.querySelector('ul');
 let sectionObj = {};
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
+let header = document.querySelector('.page__header');
+
+// Helper Functions
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
@@ -37,31 +16,21 @@ function isInViewport(element) {
   );
 }
 
+// Main Functions
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
+// Build the navbar
 function buildNav() {
   const fragment = document.createDocumentFragment();
-
   for (const section of sections) {
     const li = document.createElement('li');
     li.classList.add('menu__link');
-    // const a = document.createElement('a');
     li.textContent = section.dataset.nav;
     sectionObj[section.dataset.nav] = section.id;
-    //console.log(sectionObj);
-    // a.setAttribute('href', '#'+section.id);
-    // li.appendChild(a);
     fragment.appendChild(li);
   }
-
   navBar.appendChild(fragment);
 }
+
 // Add class 'active' to section when near top of viewport
 function addClassActive(elements) {
   for (const element of elements) {
@@ -74,7 +43,6 @@ function addClassActive(elements) {
 }
 
 // Scroll to anchor ID using scrollTO event
-
 function scroll(element) {
   highlighted.classList.remove('highlight');
   highlighted = element;
@@ -84,21 +52,32 @@ function scroll(element) {
   clickedSection.scrollIntoView({behavior:'smooth'});
 }
 
+// Events
+function setActive() {
+  document.addEventListener('scroll', () => {addClassActive(sections)});
+}
 
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+function hideNavbar() {
+  document.addEventListener('scroll', () => {
+    header.classList.remove('scroll-not-active')
+    setTimeout(() => {header.classList.add('scroll-not-active')}, 5000);
+  })
+}
 
-document.addEventListener('scroll', () => {addClassActive(sections)});
-navBar.addEventListener('click',
+function scrollToSection() {
+  navBar.addEventListener('click',
   (e) => {e.target.nodeName === 'LI' ? scroll(e.target) : null}
 );
+}
 
 // Build menu
 buildNav();
 
 // Scroll to section on link click
+scrollToSection();
 
 // Set sections as active
+setActive();
+
+// Hide nav bar when not scrolling
+hideNavbar();
